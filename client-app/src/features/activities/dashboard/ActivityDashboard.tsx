63d295e032cbf991e270c89bbad9604d65087649
Randee Shirts <rshirts@users.noreply.github.com>
2023-01-18 13:01:09 -0700

    End of section 6

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,6 +14,7 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export default function ActivityDashboard({
@@ -25,7 +26,8 @@ export default function ActivityDashboard({
         closeForm,
         openForm,
         createOrEdit,
-        deleteActivity
+        deleteActivity,
+        submitting
     }: Props) {
     return (
         <Grid>
@@ -34,6 +36,7 @@ export default function ActivityDashboard({
                     activities={activities} 
                     selectActivity={selectActivity}
                     deleteActivity={deleteActivity}
+                    submitting={submitting}
                 />
             </Grid.Column>
             <Grid.Column width='6'>
@@ -45,7 +48,7 @@ export default function ActivityDashboard({
                     /> 
                 }
                 {editMode &&
-                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
+                <ActivityForm submitting={submitting} closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
             </Grid.Column>
         </Grid>
     )
