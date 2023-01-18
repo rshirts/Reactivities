c9c1e2685441cc18589c45ce5c608f10bc41009c
Randee Shirts <rshirts@users.noreply.github.com>
2023-01-17 23:18:59 -0700

    End of Section 5

---
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -0,0 +1,38 @@
+import { Button, Item, Label } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+
+interface Props {
+    activities: Activity[];
+    deleteActivity: (id: string) => void;
+    selectActivity: (id: string) => void;
+}
+
+export default function ActivityList({activities, selectActivity, deleteActivity}: Props) {
+    return (
+        <>
+            <Item.Group divided>
+                {activities.map(activity => (
+                    <Item key={activity.id}>
+                        <Item.Content>
+                            <Item.Header as='a'>{activity.title}</Item.Header>
+                            <Item.Meta>{activity.date}</Item.Meta>
+                            <Item.Description>
+                                <div>
+                                    {activity.description}
+                                </div>
+                                <div>
+                                    {activity.city}, {activity.venue}
+                                </div>
+                            </Item.Description>
+                            <Item.Extra>
+                                <Button onClick={() => selectActivity(activity.id)} floated="right" content="View" color="blue" />
+                                <Button onClick={() => deleteActivity(activity.id)} floated="right" content="Delete" color="red" />
+                                <Label basic content={activity.category} />
+                            </Item.Extra>
+                        </Item.Content>
+                    </Item>
+                ))}
+            </Item.Group>
+        </>
+    )
+}
