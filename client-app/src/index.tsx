09c5be8344308f64bafd4d301cfbbdf96861bf71
Randee Shirts <rshirts@users.noreply.github.com>
2023-01-19 13:47:02 -0700

    End of Seciton 8

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css';
-import App from './app/layout/App';
+// import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
+import { router } from './app/router/Routes';
+import { RouterProvider } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,7 +13,8 @@ const root = ReactDOM.createRoot(
 root.render(
 //   <React.StrictMode>
     <StoreContext.Provider value={store}>
-        <App />
+        {/* <App /> */}
+        <RouterProvider router={router} />
     </StoreContext.Provider>
 
 //   </React.StrictMode>
